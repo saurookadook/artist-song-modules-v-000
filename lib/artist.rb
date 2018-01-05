a2d6f38d622f55bf5877be87e4c0bb694f42e5de
Andy Maskiell <maskiella@gmail.com>
2018-01-05 23:21:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,8 +26,8 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 
 end
